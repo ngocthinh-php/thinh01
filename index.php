6cc35183fbbf6324ce0d38f1b9077d45315d6071
ngocthinh-php <thinhb1609550@student.ctu.edu.vn>
2020-03-25 15:59:56 +0700

    add page

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello World";
-?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<form action="/form.php" method="POST">
+  <p> Ho Ten: <input type="text" name="fname" value=""></p>
+  <p> Email: <input type="text" name="mail" value=""></p>
+  <input type="submit" name="submit" value="submit">
+</form> 
+</body>
+</html>
