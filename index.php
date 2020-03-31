7b931e203344d528dd1db8bd1f14d33603f7296d
ngocthinh-php <62285771+ngocthinh-php@users.noreply.github.com>
2020-03-31 16:07:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <html>
 <body>
+<form action="/form.php" method="POST">
+  <p> Ho Ten: <input type="text" name="fname" value=""></p>
+  <p> Email: <input type="text" name="mail" value=""></p>
+  <input type="submit" name="submit" value="submit">
+</form> 
 <br><a href = setup.php>Create Database</a>
 <br><a href = add_account.php>Add Account</a>
 <br><a href = list_account.php>List Accounts </a>
