ac8c44d8ef4d8da3025b6439a2cbdc110c84b444
ngocthinh-php <thinhb1609550@student.ctu.edu.vn>
2020-04-06 16:48:28 +0700

    New commit by other

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <form action="/form.php" method="POST">
-  <p> Ho Ten: <input type="text" name="fname" value=""></p>
+  <p> Ho Ten: <input type="text" name="fname" value="Ngoc Thinh"></p>
   <p> Email: <input type="text" name="mail" value=""></p>
   <input type="submit" name="submit" value="submit">
 </form> 
