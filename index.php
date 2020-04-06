fc08136d6c2018645698b4f349845524d8f2fe18
ngocthinh-php <thinhb1609550@student.ctu.edu.vn>
2020-04-06 17:02:07 +0700

    this line is made by thinhpo1998@gmail.com

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body>
 <form action="/form.php" method="POST">
   <p> Ho Ten: <input type="text" name="fname" value="Ngoc Thinh"></p>
+// Day la text do thinhpo1998@gmail.com soan
   <p> Email: <input type="text" name="mail" value=""></p>
   <input type="submit" name="submit" value="submit">
 </form> 
